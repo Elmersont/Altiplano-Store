98de17b19f56a59d52cef5da91e2e99d4cca6436
Elmerson Torres <elmerson.t@hotmail.com>
2024-09-15 20:15:00 -0300

    configuración cors

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,17 @@
 import express from 'express';
 import routes from './routes/routes.js'
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
+app.use(cors({
+    origin: process.env.STATIC_SITE
+}))
 app.use(express.json());
-app.use(cookieParser())
-app.use('/',routes);
+app.use(cookieParser());
 app.use(express.urlencoded({extended:true}));
+app.use('/',routes);
 
 app.listen(PORT, () => console.log (`listening on port http://localhost:${PORT}`))
